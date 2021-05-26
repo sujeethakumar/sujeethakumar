2b3e46713c8d1217258ecfb1b80c3df8b7994afe
suji123 <84764758+sujeethakumar@users.noreply.github.com>
2021-05-26 17:24:11 +0530

    Create Largest.java

---
+++ Largest.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class secondlargest
+{
+    public static void main(String[] args) 
+    { 
+        int i;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for( i=0; i<n; i++)
+            arr[i] = sc.nextInt();
+                int max=arr[0];
+            Arrays.sort(arr);
+        for( i=0;i<n;i++)
+        {
+                if (arr[i] > max)
+                 max = arr[i];
+               } 
+             int max2=arr[0];
+        for( i=0;i<n;i++)
+        {
+                if(arr[i]!=max){
+                if (arr[i] > max2)
+                 max2= arr[i];}
+            }
+
+
+           System.out.println(max2);
+}
+}
